a99fd3c324b045405baa9b462150878bd27612e9
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-04 16:42:17 +0200

    Zadanie 5.1

---
+++ src/Tablica.java
@@ -0,0 +1,10 @@
+public class Tablica {
+    public static void main(String[] args) {
+        String[] tablicaImion = new String[3];
+        tablicaImion[0] = "Jan";
+        tablicaImion[1] = "Karolina";
+        tablicaImion[2] = "Tosia";
+        System.out.println(tablicaImion[0]+ " "+tablicaImion[1]+" "+ tablicaImion[2]);
+
+    }
+}
